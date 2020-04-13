7e4cf542237c40e4915ca4dccd358b7a5f3b4c64
Sachin Boob <sachin.boob@jadeglobal.com>
2020-04-13 16:13:45 +0530

    Formatting

--- app.js
+++ app.js
@@ -9,16 +9,16 @@ const notes = require('./notes.js');
 
 var user = os.userInfo();
 var res = notes.addNote();
-var concatinated_notes = notes.add('Sachin','Boob');
+var concatinated_notes = notes.add('Sachin', 'Boob');
 
 console.log(res);
 console.log(concatinated_notes);
-console.log('Result:',notes.add(1,2));
+console.log('Result:', notes.add(1, 2));
 
 console.log(_.isString(true));
 console.log(_.isString('a'));
 
-var filteredArray = _.uniq(['Sachin',1,"Sachin",1,2,3,4,'Boob']);
+var filteredArray = _.uniq(['Sachin', 1, "Sachin", 1, 2, 3, 4, 'Boob']);
 console.log(filteredArray);
 // with template string
 // fs.appendFile('greetings.txt',`Hello ${user.username} you are ${notes.age}.`,function (err){
@@ -31,18 +31,18 @@ console.log(filteredArray);
 // print all arguments passed as command line arguments
 console.log(process.argv);
 var command = process.argv[2];
-console.log('Command: ' , command);
+console.log('Command: ', command);
 console.log("Process: ", process.argv);
 console.log("Yargs: ", yargs.argv);
 
-if(command === 'add'){
+if (command === 'add') {
   console.log('Adding new note...');
-}else if (command === 'read') {
+} else if (command === 'read') {
   console.log('Reading note');
-}else if (command === 'remove') {
+} else if (command === 'remove') {
   console.log('Removing note');
-}else if(command === 'list'){
+} else if (command === 'list') {
   console.log('Listing notes...')
-}else{
+} else {
   console.log('Command not recognised...');
 }
--- notes.js
+++ notes.js
@@ -4,9 +4,9 @@ console.log('Starting notes.js');
 
 module.exports.age = 28;
 module.exports.addNote = () => {
-console.log('addNote');
-return 'New note';
+  console.log('addNote');
+  return 'New note';
 };
-module.exports.add = (note_1 , note_2) => {
+module.exports.add = (note_1, note_2) => {
   return `Part 1 -> ${note_1} : Part 2 -> ${note_2}`;
 };
